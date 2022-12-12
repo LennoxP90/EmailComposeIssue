be365bf1b6d178c4c1e7a3e6927a92a5d517acc9
Dylan Lennox <dylan.lennox@seqent.com>
2022-12-12 11:44:48 -0500

    first commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+    string recipient = "{CHANGE THIS TO REAL EMAIL}";
+
+    EmailMessage message = new()
+    {
+      Subject = "Test",
+      Body = "Test EMAIL",
+      BodyFormat = EmailBodyFormat.PlainText, 
+      To = new List<string>() { recipient.Trim() }
+    };
+
+    await Email.Default.ComposeAsync( message );
+  }
+}
+
